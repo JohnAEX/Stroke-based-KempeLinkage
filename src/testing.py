76b0640d876cc4e180cddd18fd1b47be282efbb1
JohnAEX <grenda.jonathan@gmail.com>
2022-01-17 13:24:13 +0100

    Added counter parallelogram and multi-geometry handling

--- src/testing.py
+++ src/testing.py
@@ -7,7 +7,13 @@ if __name__ == '__main__':
     p3 = Point(0, 2)
     p4 = Point(1, 1)
     base = Rhombus(p1, p2, p3, p4)
-    print(geometry_to_expressions(base))
+    print(geometry_to_expression(base))
+
+    cpara = CounterParallelogram(p1, p2, p3, p4)
+    print(geometry_to_expression(cpara))
+
+    print("Combined:")
+    print(geometries_to_expression((base, cpara)))
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from pyslvs import VPoint, VJoint, VLink
 
 
@@ -13,7 +16,7 @@ def rhombus_link(points):
     link_amount = 4
     counter = 0
     while counter < link_amount:
-        links.append(VLink("Link" + str(counter), "blue", list()))
+        links.append(VLink(generate_name("Link", counter), "blue", list()))
         counter += 1
     # todo: how do we dynamically add and remove links ?
     points[0].set_links(['ground', links[0].name, links[3].name])
@@ -21,14 +24,23 @@ def rhombus_link(points):
     points[1].set_links([links[0].name, links[1].name])
     points[2].set_links([links[1].name, links[2].name])
 
-    # for index, p in enumerate(points):
-    #     if index == len(points) - 1:
-    #         link = VLink("Link" + str(index), "orange", [p, 1])
-    #         # p.set_links([link.name])
-    #     else:
-    #         link = VLink("Link" + str(index), "orange", [])
-    #         # p.set_links([link.name])
-    #
+
+def counter_parallelogram_link(points):
+    links = []
+    link_amount = 4
+    counter = 0
+    while counter < link_amount:
+        links.append(VLink(generate_name("Link", counter), "blue", list()))
+        counter += 1
+    # todo: how do we dynamically add and remove links ?
+    points[0].set_links(['ground', links[0].name, links[2].name])
+    points[3].set_links([links[3].name, links[1].name])
+    points[1].set_links([links[0].name, links[1].name])
+    points[2].set_links([links[3].name, links[2].name])
+
+
+def generate_name(base, counter):
+    return base + "_" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=3)) + "_" + str(counter)
 
 
 class Rhombus:
@@ -40,28 +52,36 @@ class Rhombus:
         rhombus_link(self.points)
 
 
-# linkage will be different from Rhombus, linkage not implemented yet
 class CounterParallelogram:
     points = []
 
     def __init__(self, p1, p2, p3, p4):
         self.points.extend([point_to_vpoint(p1), point_to_vpoint(p2), point_to_vpoint(p3), point_to_vpoint(p4)])
+        counter_parallelogram_link(self.points)
 
 
 # example: VPoint(('ground', 'L1'), 0, 0.0, [[-67.38, 36.13], [0.0, 0.0]])
 def point_to_vpoint(point):
-    # todo: implement linkage
     # todo: differentiate between R / P / RP Joints
     point = VPoint('', VJoint.R, 0, "blue", point.x, point.y)
     print()
     return point
 
 
-def geometry_to_expressions(geo):
+def geometry_to_expression(geo):
     """ Return a valid Mechanism-Expression based on the VPoints in a Geometry object
     that can then be imported into pyslvs """
     expression = "M["
     for p in geo.points:
         expression += "\n" + p.expr() + ","
+    expression += "\n]"
+    return expression
+
+
+def geometries_to_expression(geos):
+    expression = "M["
+    for geo in geos:
+        for p in geo.points:
+            expression += "\n" + p.expr() + ","
     expression += "\n]"
     return expression
